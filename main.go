eaf7e4ffad00b57d8a27d1b4dc8a3806d49e8370
ryanCool <rexq4@hotmail.com>
2023-05-17 01:28:54 +0800

    Update main.go

--- main.go
+++ main.go
@@ -9,43 +9,34 @@ import (
 )
 
 const (
-	//设置常量 分别设置chromedriver.exe的地址和本地调用端口
 	seleniumPath = `/Users/ryan/Downloads/chromedriver`
 	port         = 9515
 )
 
 func main() {
-	//1.开启selenium服务
-	//设置selium服务的选项,设置为空。根据需要设置。
+
 	ops := []selenium.ServiceOption{}
 	service, err := selenium.NewChromeDriverService(seleniumPath, port, ops...)
 	if err != nil {
 		fmt.Printf("Error starting the ChromeDriver server: %v", err)
 	}
-	//延迟关闭服务
 	defer service.Stop()
 
 
 
-	//2.调用浏览器
-	//设置浏览器兼容性，我们设置浏览器名称为chrome
 	caps := selenium.Capabilities{
 		"browserName": "chrome",
 
 	}
 	caps.SetLogLevel(log.Performance,log.All)
-	//调用浏览器urlPrefix: 测试参考：DefaultURLPrefix = "http://127.0.0.1:4444/wd/hub"
 	wd, err := selenium.NewRemote(caps, "http://127.0.0.1:9515/wd/hub")
 	if err != nil {
 		panic(err)
 	}
-	//延迟退出chrome
 	defer wd.Quit()
 
 
 
-	//3.对页面元素进行操作
-	//获取百度页面
 	if err := wd.Get("https://www.facebook.com/ads/library/?active_status=all&ad_type=all&country=ALL&view_all_page_id=341166659618884&sort_data[direction]=desc&sort_data[mode]=relevancy_monthly_grouped"); err != nil {
 		panic(err)
 	}
@@ -63,7 +54,6 @@ func main() {
 	for _,log :=range logs{
 		fmt.Println("log=",log.Message)
 	}
-	//睡眠20秒后退出
 	time.Sleep(60 * time.Second)
 }
 
